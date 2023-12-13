1f6d29a1fc313f56dcce65ce2d06b293289a3acd
Luis Pabon <turkeyfried@gmail.com>
2023-12-12 19:25:57 -0500

    Update Dashboard.vue
    refreshing myself on vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,20 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, useForm } from '@inertiajs/vue3';
+import TextInput from '@/Components/TextInput.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+
+var listText = ""
+var listName = ""
+function saveList(input) {
+    console.log("saving...")
+}
+
+const form = useForm({
+    name: '',
+    text: '',
+    open: false,
+});
 </script>
 
 <template>
@@ -12,11 +26,30 @@ import { Head } from '@inertiajs/vue3';
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+            <form @submit.prevent="submit">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
+                            <!-- <li v-for="item in items">
+                            {{ item.message }}
+                            </li> -->
+                            <textarea v-model="listText" placeholder="List contents"></textarea>
+                        </div>
+                    </div>
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <TextInput
+                        id="list-name"
+                        class="mt-1 block w-full"
+                        v-model="listName"
+                        autofocus
+                        placeholder="List name"
+                    />
+                    <PrimaryButton @click="saveList()" class="ms-4">
+                        Save
+                    </PrimaryButton>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </AuthenticatedLayout>
 </template>
